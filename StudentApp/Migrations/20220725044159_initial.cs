056ae27f2547bbfd1867627d4ffe86b87474251a
unknown <ishanmadhawa09@gmail.com>
2022-07-25 10:49:14 +0530

    initial migration created

---
+++ StudentApp/Migrations/20220725044159_initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentApp.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(20)", nullable: false),
+                    StudentAge = table.Column<int>(type: "int", nullable: false),
+                    StudentAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentPhone = table.Column<int>(type: "int", nullable: false),
+                    StudentEmail = table.Column<int>(type: "int", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
